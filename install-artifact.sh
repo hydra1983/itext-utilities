b329a22f5bca3d6339bcfbc6456df603acfb4a39
Edison Guo <hydra1983@gmail.com>
2015-03-02 09:51:00 +0800

    Fix group id

--- install-artifact.sh
+++ install-artifact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mvn install:install-file -Dfile=./dists/iTextAsian-2.1.7.jar \
-  -DgroupId='com.lowgie' \
+  -DgroupId='com.lowagie' \
   -DartifactId='iTextAsian' \
   -Dversion='2.1.7' \
   -Dpackaging='jar' \
